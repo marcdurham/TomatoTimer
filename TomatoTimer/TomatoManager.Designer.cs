ad9c025bef043ae2a869e2b25eb84afe10f2db30
Marc Durham <marcdurham@gmail.com>
2018-09-06 11:04:36 +0800

    Fix tick increment

--- TomatoTimer/TomatoManager.Designer.cs
+++ TomatoTimer/TomatoManager.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // timer
             // 
-            this.timer.Interval = 2000;
+            this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // startButton
