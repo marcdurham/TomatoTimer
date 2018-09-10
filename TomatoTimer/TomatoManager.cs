8ba1bcad80befe07656a3f258b3a7eb7d13673bd
Marc Durham <marcdurham@gmail.com>
2018-09-10 18:54:21 +0800

    Remove old icon code

--- TomatoTimer/TomatoManager.cs
+++ TomatoTimer/TomatoManager.cs
@@ -9,17 +9,7 @@ namespace TomatoTimer
     public partial class TomatoManagerForm : Form
     {
         const int SecondsPerMinute = 60;
-        const int TwentyFive = 25 * SecondsPerMinute;
-        const int Twenty = 20 * SecondsPerMinute;
-        const int Fifteen = 15 * SecondsPerMinute;
-        const int Ten = 10 * SecondsPerMinute;
-        const int Five = 5 * SecondsPerMinute;
-        const int Four = 4 * SecondsPerMinute;
-        const int Three = 3 * SecondsPerMinute;
-        const int Two = 2 * SecondsPerMinute;
-        const int One = 1 * SecondsPerMinute;
-        const int Zero = 0;
-        const int Duration = TwentyFive;
+        const int Duration = 25 * SecondsPerMinute;
 
         int ticks = Duration;
 
@@ -53,62 +43,20 @@ namespace TomatoTimer
 
         void DecrementTimer()
         {
-            switch (ticks)
-            {
-                case TwentyFive:
-                    notifyIcon.Icon = Properties.Resources.TwentyFive;
-                    break;
-                case Twenty:
-                    notifyIcon.Icon = Properties.Resources.Twenty;
-                    break;
-                case Fifteen:
-                    notifyIcon.Icon = Properties.Resources.Fifteen;
-                    break;
-                case Ten:
-                    notifyIcon.Icon = Properties.Resources.Ten;
-                    break;
-                case Five:
-                    notifyIcon.Icon = Properties.Resources.Five;
-                    break;
-                case Four:
-                    notifyIcon.Icon = Properties.Resources.Four;
-                    break;
-                case Three:
-                    notifyIcon.Icon = Properties.Resources.Three;
-                    break;
-                case Two:
-                    notifyIcon.Icon = Properties.Resources.Two;
-                    break;
-                case One:
-                    notifyIcon.Icon = Properties.Resources.One;
-                    break;
-                case Zero:
-                    notifyIcon.Icon = Properties.Resources.Tomato;
-                    Done();
-                    break;
-            }
-
             int minutes = ticks / SecondsPerMinute;
             int seconds = ticks % SecondsPerMinute;
 
             notifyIcon.Text = $"Remaining {minutes}:{seconds}";
 
-            Bitmap bmp = new Bitmap(16, 16, PixelFormat.Format24bppRgb);
-            //var font = new Font(FontFamily.GenericSansSerif, 10.0);
-            var font = new Font("Sans Serif", 10.0f);
+            var bmp = new Bitmap(16, 16, PixelFormat.Format24bppRgb);
+            var font = new Font(FontFamily.GenericSansSerif, 6.0f);
+
             using (Graphics g = Graphics.FromImage(bmp))
             {
-                //g.FillEllipse(Brushes.Red, 0, 0, 16, 16);
-                //g.FillRectangle(Brushes.White, 4, 6, 8, 4);
-                //g.DrawRectangle(Pens.White, 1, 1, 14, 14);
-                string display = "TT";
-                if (ticks % 2 == 0)
-                    display = $"{seconds}";
-                else
-                    display = $"{minutes}";
-
-                g.DrawString(display, font, Brushes.White, 0.0f, 0.0f);
+                g.DrawString($"{minutes}", font, Brushes.White, 0.0f, 0.0f);
+                g.DrawString($"{seconds:00}", font, Brushes.White, 0.0f, 7.0f);
             }
+
             notifyIcon.Icon = Converter.BitmapToIcon(bmp);
 
             ticks--;
